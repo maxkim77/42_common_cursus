ad6fabc8d40c32dc9168721bc733739da015a74b
Jeongwon Kim <jeongkim@c3r10s2.42gyeongsan.kr>
2025-04-04 23:17:34 +0900

    Add

--- libft/libft.h
+++ libft/libft.h
@@ -3,9 +3,21 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
 
+typedef struct s_list
+{
+	void		*content;
+	struct s_list *next;
+	t_list;
+}
+
+int ft_isalnum(int c);
 int	ft_isalpha(int c);
+int	ft_isascii(int c);
 int	ft_isdigit(int c);
 int	ft_isprint(int c);
+size_t ft_strlen(const char *s);
 
 #endif
