099db871d12bb8b8ea575378513485ded28db794
maxkim77 <maxkim7723@gmail.com>
2025-05-08 20:15:26 +0900

    add

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -12,14 +12,14 @@
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *str, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (s[i])
 	{
-		write(fd, &str[i], 1);
+		write(fd, &s[i], 1);
 		i++;
 	}
 	write(fd, "\n", 1);
