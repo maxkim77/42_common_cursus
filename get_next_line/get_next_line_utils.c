76220ac57428b97db710ee432d3d1289f57c9041
maxkim77 <maxkim7723@gmail.com>
2025-06-07 15:03:35 +0900

    add

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,72 @@
+#include "get_next_line.h"
+
+char    *ft_strdup(char *s1)
+{
+    char        *dest;
+    unsigned int    i;
+
+    dest = (char *) malloc(ft_strlen(s1) + 1);
+    if(!dest)
+        return (NULL);
+    i = 0;
+    while (s1[i])
+    {
+        dest[i] = s1[i];
+        i++;
+    }
+    dest[i] = 0;
+    return (dest);
+}
+
+size_t ft_strlen(char *s)
+{
+    int i;
+    
+    i = 0;
+    while (s1[i])
+        i++;
+    return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*res;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	res = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	while (s1[i])
+		res[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i++];
+	res[j] = '\0';
+	return (res);
+}
