7d4b31a011e345b98f8468082190a5a28915c47d
maxkim77 <maxkim7723@gmail.com>
2025-06-07 15:29:46 +0900

    add

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,44 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeongkim <jeongkim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/07 12:00:00 by jeongkim          #+#    #+#             */
+/*   Updated: 2025/06/07 12:00:00 by jeongkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char    *ft_strdup(char *s1)
+char	*ft_strdup(char *s1)
 {
-    char        *dest;
-    unsigned int    i;
+	char			*dest;
+	unsigned int	i;
 
-    dest = (char *) malloc(ft_strlen(s1) + 1);
-    if(!dest)
-        return (NULL);
-    i = 0;
-    while (s1[i])
-    {
-        dest[i] = s1[i];
-        i++;
-    }
-    dest[i] = 0;
-    return (dest);
+	dest = (char *)malloc(ft_strlen(s1) + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
-size_t ft_strlen(char *s)
+size_t	ft_strlen(char *s)
 {
-    int i;
-    
-    i = 0;
-    while (s1[i])
-        i++;
-    return (i);
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	char	*str;
 
 	if (!s)
 		return (NULL);
-	if (start > ft_strlen(s))
+	if (start > ft_strlen((char *)s))
 		return (ft_strdup(""));
-	if (len > ft_strlen(s + start))
-		len = ft_strlen(s + start);
+	if (len > ft_strlen((char *)s + start))
+		len = ft_strlen((char *)s + start);
 	str = ft_calloc(len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
@@ -59,7 +71,8 @@ char	*ft_strjoin(const char *s1, const char *s2)
 
 	i = 0;
 	j = 0;
-	res = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	res = (char *)malloc((ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1)
+			* sizeof(char));
 	if (!res)
 		return (NULL);
 	while (s1[i])
@@ -69,4 +82,20 @@ char	*ft_strjoin(const char *s1, const char *s2)
 		res[j++] = s2[i++];
 	res[j] = '\0';
 	return (res);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+	size_t	i;
+	char	*tmp;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	tmp = (char *)ptr;
+	i = 0;
+	while (i < count * size)
+		tmp[i++] = 0;
+	return (ptr);
 }
