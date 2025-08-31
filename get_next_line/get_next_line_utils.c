3389cc4baa244ea2154eb707aa40676141762aa5
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2025-08-31 14:43:21 +0900

    Update get_next_line_utils.c

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -12,90 +12,65 @@
 
 #include "get_next_line.h"
 
-char	*ft_strdup(char *s1)
-{
-	char			*dest;
-	unsigned int	i;
-
-	dest = (char *)malloc(ft_strlen(s1) + 1);
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		dest[i] = s1[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
 size_t	ft_strlen(char *s)
 {
-	int	i;
+	size_t	i;
 
+	if (!s)
+		return (0);
 	i = 0;
 	while (s[i])
 		i++;
 	return (i);
 }
 
-char	*ft_substr(const char *s, unsigned int start, size_t len)
+char	*ft_strdup(char *s1)
 {
+	size_t	len;
 	size_t	i;
-	char	*str;
+	char	*cpy;
 
-	if (!s)
+	if (!s1)
 		return (NULL);
-	if (start > ft_strlen((char *)s))
-		return (ft_strdup(""));
-	if (len > ft_strlen((char *)s + start))
-		len = ft_strlen((char *)s + start);
-	str = ft_calloc(len + 1, sizeof(char));
-	if (!str)
+	len = ft_strlen(s1);
+	cpy = (char *)malloc(len + 1);
+	if (!cpy)
 		return (NULL);
 	i = 0;
 	while (i < len)
 	{
-		str[i] = s[start + i];
+		cpy[i] = s1[i];
 		i++;
 	}
-	return (str);
+	cpy[i] = '\0';
+	return (cpy);
 }
 
-char	*ft_strjoin(const char *s1, const char *s2)
+char	*ft_strjoin(const char *left, const char *right)
 {
-	char	*res;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	res = (char *)malloc((ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1)
-			* sizeof(char));
-	if (!res)
-		return (NULL);
-	while (s1[i])
-		res[j++] = s1[i++];
-	i = 0;
-	while (s2[i])
-		res[j++] = s2[i++];
-	res[j] = '\0';
-	return (res);
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void	*ptr;
+	size_t	ll;
+	size_t	rr;
 	size_t	i;
-	char	*tmp;
+	size_t	j;
+	char	*out;
 
-	ptr = malloc(count * size);
-	if (!ptr)
+	ll = ft_strlen((char *)left);
+	rr = ft_strlen((char *)right);
+	out = (char *)malloc(ll + rr + 1);
+	if (!out)
 		return (NULL);
-	tmp = (char *)ptr;
 	i = 0;
-	while (i < count * size)
-		tmp[i++] = 0;
-	return (ptr);
+	while (i < ll)
+	{
+		out[i] = left[i];
+		i++;
+	}
+	j = 0;
+	while (j < rr)
+	{
+		out[i + j] = right[j];
+		j++;
+	}
+	out[i + j] = '\0';
+	return (out);
 }
