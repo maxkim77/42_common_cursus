c18806d2f09545a95d317e1f266d380ad202b94e
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2025-08-31 14:42:59 +0900

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -19,13 +19,10 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
 
 char	*get_next_line(int fd);
-char	*ft_strdup(char *s);
 size_t	ft_strlen(char *s);
-char	*ft_substr(const char *s, unsigned int start, size_t len);
-char	*ft_strjoin(const char *s1, const char *s2);
-void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(char *s1);
+char	*ft_strjoin(const char *left, const char *right);
 
 #endif
